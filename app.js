1b52371f6a9e2e76787f34ae38d02389d0727cab
Tonya-Ostapiuk <116578205+Tonya-Ostapiuk@users.noreply.github.com>
2023-04-28 15:52:21 +0300

    “hw05-avatars”

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
-
+// const multer = require('multer')
 const contactsRouter = require('./routes/api/contacts')
 const authRouter = require('./routes/auth/user')
 
@@ -12,6 +12,7 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
+app.use(express.static("public"))
 
 app.use('/auth', authRouter)
 app.use('/api/contacts', contactsRouter)
--- routes/auth/user.js
+++ routes/auth/user.js
@@ -1,24 +1,68 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
-const {createHash, createHttpException, createJWT} = require("../../servceis/index");
+const path = require("path");
+const gravatar = require("gravatar");
+const fsp = require('fs/promises')
+const Jimp = require("jimp");
+const {
+  createHash,
+  createHttpException,
+  createJWT,
+} = require("../../servceis/index");
 const { UserModel } = require("../../database/user.model");
 const router = express.Router();
 const crypto = require("crypto");
-const { userAuthMiddeleware } = require("../../middlewares/user-auth.middleware");
+const {
+  userAuthMiddeleware,
+} = require("../../middlewares/user-auth.middleware");
+const { upload } = require("../../middlewares/upload");
 
+
+
+const publicPath = path.join(__dirname, "../../", "public", "avatars");
+
+router.patch("/avatars", userAuthMiddeleware, upload.single("avatar"), async (req, res) => {
+  const { path: tempUpload, originalname } = req.file;
+  const user = req.user;
+  const fileName = `${user._id}_${originalname}`;
+
+  try {
+    const resultUpload = path.join(publicPath, fileName);
+
+    Jimp.read(tempUpload, (err, avatar) => {
+      if (err) throw err;
+      avatar
+        .resize(250, 250)
+        .write(tempUpload); 
+    });
+
+    await fsp.rename(tempUpload, resultUpload);
+    
+    const avatarURL = path.join( "avatars", fileName);
+    await UserModel.findByIdAndUpdate(user._id, { avatarURL });
+  
+    res.status(201).json({avatarURL});
+  } catch (error) {
+    await fsp.unlink(tempUpload);
+    throw error;
+    
+  }
+});
 
 router.post("/sign-up", async (req, res, next) => {
   try {
     const { email, password } = req.body;
 
-    const passwordHash = await createHash( password );
+    const passwordHash = await createHash(password);
+    const avatarURL = gravatar.url(email);
+    console.log(avatarURL);
 
     const existingUser = await UserModel.findOne({ email });
     if (existingUser) {
-      throw createHttpException("Email in use", 409);  
+      throw createHttpException("Email in use", 409);
     }
 
-    const newUser = await UserModel.create({ email, passwordHash });
+    const newUser = await UserModel.create({ email, passwordHash, avatarURL });
 
     const sessionKey = crypto.randomUUID();
     await UserModel.findByIdAndUpdate(newUser._id, { sessionKey });
@@ -29,8 +73,12 @@ router.post("/sign-up", async (req, res, next) => {
     });
 
     res.status(201).json({
-      user: { email: newUser.email, subscription: newUser.subscription },
-  });
+      user: {
+        email: newUser.email,
+        subscription: newUser.subscription,
+        avatarURL: avatarURL,
+      },
+    });
   } catch (error) {
     next(error);
   }
@@ -62,7 +110,7 @@ router.post("/sign-in", async (req, res, next) => {
     res.status(201).json({
       token: accessJWT,
       user: { email: user.email, subscription: user.subscription },
-  });
+    });
   } catch (error) {
     next(error);
   }
@@ -70,7 +118,7 @@ router.post("/sign-in", async (req, res, next) => {
 
 router.post("/logout", userAuthMiddeleware, async (req, res, next) => {
   try {
-    const user = req.user
+    const user = req.user;
     // console.log(req.user);
     await UserModel.findByIdAndUpdate(user._id, { sessionKey: null });
     if (!user) {
@@ -78,11 +126,9 @@ router.post("/logout", userAuthMiddeleware, async (req, res, next) => {
     }
 
     res.status(204).json("Not authorized 1.2");
-
   } catch (error) {
     next(error);
   }
 });
-
 
 module.exports = router;
